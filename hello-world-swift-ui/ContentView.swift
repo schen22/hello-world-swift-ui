8c53242b6742a56bdfde83be4ac7b2af2feedce6
schen22 <sarahchen22@gmail.com>
2019-09-22 23:33:54 -0700

    Initial Commit

---
+++ hello-world-swift-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-world-swift-ui
+//
+//  Created by Sarah Chen on 9/22/19.
+//  Copyright © 2019 Sarah Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
