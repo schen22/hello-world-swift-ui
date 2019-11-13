8b9d8b377b98007931af174a164e1d24de8ee7b5
schen22 <sarahchen22@gmail.com>
2019-11-12 23:22:52 -0800

    initial commit

--- hello-world-swift-ui/ContentView.swift
+++ hello-world-swift-ui/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var keyboardInput: String = ""
+    let textToType = "When I went to the suburban part of Piazzas, Italy, I assumed I would find these pizazz pizzas. Alas, all I found was obstinance which made my foramen close up."
+    var verifiedInput: String = ""
+    var currentWord: String = ""
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: HorizontalAlignment.center) {
+            Text("Texting speed")
+                .font(.title)
+            Text(textToType)
+                .lineLimit(nil)
+                .frame(minWidth: 0, idealWidth:20, maxWidth: .infinity, minHeight: 0, idealHeight: nil, maxHeight: .infinity, alignment: Alignment.center)
+            TextField("Type sentence here", text: $keyboardInput)
+            Text("\(keyboardInput)")
+
+        }
+    .padding()
+    }
+    
+    func updateVerifiedInput() {
+//        let words = keyboardInput.split(separator: " ")
+//        for word in words {
+////            checkWord(word, )
+//        }
+    }
+    
+    func checkWord(_ word: String, _ expected: String) -> Text
+    {
+        if (word != expected) {
+            return Text(word)
+                .foregroundColor(Color.red)
+        }
+        return Text(word)
     }
 }
 
